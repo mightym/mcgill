cd1d2217ef9d4eae5bbf1b48a1c0f8bd05ae6769
Mark Wirblich <mark@wirblich.com>
2017-01-18 12:23:45 +0100

    Updated static urls

--- src/project_name/project_name/urls.py
+++ src/project_name/project_name/urls.py
@@ -3,9 +3,9 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from django.conf import settings
 from django.conf.urls import *
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
 
 admin.autodiscover()
 
@@ -16,7 +16,7 @@ urlpatterns = [
 # This is only needed when using runserver.
 if settings.DEBUG:
     urlpatterns += [
-        url(r'^media/(?P<path>.*)$', django.views.static.serve,
+        url(r'^media/(?P<path>.*)$', serve,
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
         ]
     urlpatterns += staticfiles_urlpatterns()
